8e4dafdf3bac8e0dfe1f7cf21d0748c569f4ebb0
egiam <ezegiampaoli@gmail.com>
2022-05-14 21:00:14 -0300

    Responsive deisgn

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 document.getElementById("player").addEventListener("click", sumarPuntos);
 
 puntos = 0;
-tiempo = 10;
+tiempo = 30;
 necesarios = 30;
 
 function sumarPuntos() {
+    widthX = window.innerWidth * 0.9;
     puntos++;
     document.getElementById("puntos").innerHTML =
         "Puntos: <b>" + puntos + "/" + necesarios + "  </b>";
     randNum = Math.round(Math.random() * 500);
-    randNum2 = Math.round(Math.random() * 500);
+    randNum2 = Math.round(Math.random() * widthX);
     document.getElementById("player").style.marginTop = randNum + "px";
     document.getElementById("player").style.marginLeft = randNum2 + "px";
     if (puntos == 30) {
         alert("You Win!!");
-        tiempo = 10;
+        tiempo = 30;
         puntos = 0;
     }
 }
@@ -25,7 +26,7 @@ function restarTiempo() {
         "&nbsp;&nbsp;&nbsp;Tiempo: " + tiempo;
     if (tiempo == 0) {
         alert("Game Over. You Lose!");
-        tiempo = 10;
+        tiempo = 30;
         puntos = 0;
     }
 }
